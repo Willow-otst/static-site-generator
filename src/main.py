6c1e8a3f56de20d91959601ff814a9d8e7d1b577
Willow <NONE>
2025-03-10 01:36:11 -0500

    added markdown-to-textnode processing and markdown-to-textnode tests

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from textnode import TextNode, TextType
 
+from markdown_to_textnode import markdown_to_textnode
 def main():
     ## Dummy Code for Testing
     # node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
